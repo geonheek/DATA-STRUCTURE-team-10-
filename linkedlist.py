41132223ef57d26db841a986b994918c4142d76a
김건희 <101076201+geonheek@users.noreply.github.com>
2022-06-21 22:23:30 +0900

    최종본

--- linkedlist.py
+++ linkedlist.py
@@ -1,40 +1,46 @@
 # Node 정의
 class Node(object):
-    def __init__(self, data, next=None):
+    def __init__(self, data,  next=None):
         self.data = data
         self.next = next
+
 
 class LinkedList(object):
     def __init__(self):
         self.head = Node(None)
         self.size = 0
 
-    def listSize(self):
+    def listSize(self): # O(1)
         return self.size
 
-    def is_empty(self):
-        return self.size ==0
-
-    def selectNode(self,a):
-        target = self.head
-        b = 0
-        #for i in range(self.size):
-        while target:
-            if a in target.data:
-                print(target.data)
-                b+=1
-                target = target.next
-            else: target = target.next
-
-        if b ==0:
-            print("찾은 데이터 없음\n")
-
+    def is_empty(self): # O(1)
+        if self.size != 0:
+            return False
         else:
-            print(b, " 개 발견\n") 
+            return True
 
+    def selectNode(self, idx): # O(n) (아래와 같음)
+        if idx >= self.size:
+            print("Index Error")
+            return None
+        if idx == 0:
+            return self.head
+        else:
+            target = self.head
+            for cnt in range(idx):
+                target = target.next
+            return target
+
+    # appendleft
+    def appendleft(self, value): # O(1)
+        if self.is_empty():
+            self.head = Node(value)
+        else:
+            self.head = Node(value, self.head)
+        self.size += 1
 
     # append
-    def append(self, value):
+    def append(self, value): # O(n) (사실상 개인정보의 개수와 같으므로 상수시간이라 봐도 무방함)
         if self.is_empty():
             self.head = Node(value)
             self.size += 1
@@ -94,15 +100,3 @@ class LinkedList(object):
                 print(target.data)
                 target = target.next
 
-
-mylist = LinkedList()
-mylist.append("홍길동 111 ddd")
-mylist.append("홍길동 222 ddd")
-mylist.append("김건희 329 kkk")
-mylist.append("김건희 333 kkk")
-mylist.append("김건희 444 kkk")
-mylist.printlist()
-print(mylist.listSize())
-a = str(input())
-mylist.selectNode(a)
-print(mylist.is_empty())
